cf8c76bca28292934dc6cd925927a558bb0385b9
Jason Hsu <jhsu@Jasons-MacBook-Air.local>
2014-05-03 09:54:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyclo6::Application.config.secret_key_base = 'fdc26e5f4704180c5c3f77dbc9d784a81c3e04c3efebc191f35eaf67e7ab7c86010b96145010b3ab14f820b5d09364801b27254385e3b64404f2dd08d29e8f66'
